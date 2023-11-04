944a9df072f0a38cc283f4b2ae63292294e14b49
lizunlong <lizunlong@gmail.com>
2023-11-04 15:14:57 +0800

    fix

--- components/chat/input.tsx
+++ components/chat/input.tsx
@@ -8,11 +8,13 @@ interface IProps {
 }
 
 export default function ChatInput({ id }: IProps) {
-  const { input, handleInputChange, append, setInput } = useChat({ id })
+  const { input, handleInputChange, append, setInput, isLoading } = useChat({
+    id,
+  })
 
   const onSubmit = (content: string) => {
     const q = content.trim()
-    if (q === '') return
+    if (q === '' || isLoading) return
     append({
       role: 'user',
       content: q,
