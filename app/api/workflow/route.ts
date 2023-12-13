cd929e54dc40bb22a9db32fd2c49acdcecccd780
lizunlong <lizunlong@gmail.com>
2023-12-13 14:22:38 +0800

    prompt

--- app/api/workflow/route.ts
+++ app/api/workflow/route.ts
@@ -16,22 +16,6 @@ const openai = new OpenAI({
 // gpt-4-1106-preview
 const model = 'gpt-3.5-turbo-1106'
 
-const variable_rules = `[IMPORTANT] return in json mode.
-
-json structure:
-
-{ "assistant_response": "your response", "variables": {required_variables}, "done": false|true }
-
-here is the rules:
-
-- 'assistant_response' is the COMPLETE response you want to return to the user, do not end the response with semicolon.
-- 'variables' is the list of variables and the 'value' field should be filled based on the info collected from user and your correction (but not from your question).
-- only the variables related with last two messages should be put into 'variables'.
-- remove description field from 'variables' before response.
-- remove the item from 'variables' which 'value' field is empty (such as empty string or array) before response.
-- 'done' is a boolean value, true means all of the steps are completed, false means there are some steps not completed.
-- check the rules again before response.`
-
 export async function POST(req: NextRequest) {
   try {
     const { code, config, messages: _messages } = await req.json()
@@ -52,7 +36,8 @@ export async function POST(req: NextRequest) {
 
     const currentStep = workflow.find((w) => !w.done)
     if (currentStep && currentStep.system_prompt) {
-      let instruction = `${currentStep.system_prompt}` + variable_rules
+      let instruction = `${currentStep.system_prompt}`
+
       if (instruction.includes('{required_variables}')) {
         let variables_text = ''
         if (currentStep.required_variables) {
@@ -61,7 +46,6 @@ export async function POST(req: NextRequest) {
           )
           variables_text += `${JSON.stringify(required_variables)}`
         }
-        console.log('variables_text:', variables_text)
         if (variables_text) {
           instruction = instruction.replace(
             '{required_variables}',
@@ -78,7 +62,8 @@ export async function POST(req: NextRequest) {
         }
       }
 
-      console.log('instruction:', instruction)
+      // console.log('instruction:', instruction)
+
       systemMessages.push({
         role: 'system',
         content: instruction,
@@ -95,7 +80,7 @@ export async function POST(req: NextRequest) {
 
     const messages = [...systemMessages, ..._messages]
 
-    console.log('systemMessages:', systemMessages)
+    // console.log('systemMessages:', systemMessages)
 
     const response = await openai.chat.completions.create({
       model,
