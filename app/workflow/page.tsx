60f66cf4ca939b82edffcd26ffb41028da433d79
lizunlong <lizunlong@gmail.com>
2023-12-14 14:34:05 +0800

    apply button

--- app/workflow/page.tsx
+++ app/workflow/page.tsx
@@ -1,15 +1,17 @@
 'use client'
 
 import * as React from 'react'
-import Editor from '@monaco-editor/react'
+import Editor, { Monaco } from '@monaco-editor/react'
 import { Message } from 'ai'
 import { useChat } from 'ai/react'
 import { isArray, isEmpty } from 'lodash'
+import { editor } from 'monaco-editor'
 import { useImmer } from 'use-immer'
 
 import { IConfig } from '@/lib/types'
 import { getCode } from '@/lib/utils'
 import { safePartialParse } from '@/lib/utils/parser'
+import { Button } from '@/components/ui/button'
 import ChatInput from '@/components/chat/input'
 import Markdown from '@/components/chat/markdown'
 
@@ -28,21 +30,29 @@ export default function Page() {
 function Chat() {
   const [config, setConfig] = useImmer<IConfig>(CONFIG)
 
-  const { messages, input, handleInputChange, append, setInput, isLoading } =
-    useChat({
-      id: 'workflow',
-      api: '/api/workflow',
-      body: {
-        code,
-        config,
-      },
-      onFinish: (message) => {
-        console.log('onFinish:', message)
-      },
-      onError: (err) => {
-        alert(err.message)
-      },
-    })
+  const {
+    messages,
+    input,
+    handleInputChange,
+    append,
+    setInput,
+    isLoading,
+    stop,
+    setMessages,
+  } = useChat({
+    id: 'workflow',
+    api: '/api/workflow',
+    body: {
+      code,
+      config,
+    },
+    onFinish: (message) => {
+      console.log('onFinish:', message)
+    },
+    onError: (err) => {
+      alert(err.message)
+    },
+  })
 
   const latestAssistantResponse = React.useMemo(() => {
     const f = messages.filter((m) => m.role === 'assistant')
@@ -79,16 +89,35 @@ function Chat() {
     }
   }, [latestAssistantResponse, setConfig])
 
+  const editorRef = React.useRef<null | editor.IStandaloneCodeEditor>(null)
+  function handleEditorDidMount(
+    editor: editor.IStandaloneCodeEditor,
+    monaco: Monaco
+  ) {
+    editorRef.current = editor
+  }
+
+  const handleApply = React.useCallback(() => {
+    stop()
+    setMessages([])
+    const config = editorRef.current?.getValue()
+    if (config) setConfig(JSON.parse(config))
+  }, [stop, setMessages, setConfig])
+
   return (
     <div className="grid grid-cols-3 gap-4 p-4">
-      <div className="col-span-1">
+      <div className="col-span-1 flex flex-col gap-4">
         <Editor
           className="overflow-hidden rounded-md border"
-          height="calc(100vh - 40px)"
+          height="calc(100vh - 80px)"
           defaultLanguage="json"
           defaultValue={JSON.stringify(CONFIG, null, 2)}
+          onMount={handleEditorDidMount}
           options={{ wordWrap: 'on' }}
         />
+        <Button variant="outline" onClick={handleApply}>
+          Apply Workflow
+        </Button>
       </div>
       <div className="col-span-1 flex flex-col gap-4 rounded-md border p-4">
         <ChatInput
@@ -102,6 +131,17 @@ function Chat() {
         <ChatList messages={messages} />
       </div>
       <div className="col-span-1 flex flex-col gap-4">
+        <div className="rounded-md border p-4">
+          <ol className="prose">
+            {config.workflow.map((w, idx) => (
+              <li key={idx} className="line-clamp-3">
+                <span className="mr-2">Step {idx + 1}:</span>
+                {w.done && <span className="mr-2">✅</span>}
+                <span>{w.system_prompt}</span>
+              </li>
+            ))}
+          </ol>
+        </div>
         {filledVariables.length > 0 && (
           <div className="rounded-md border p-4">
             <ul className="prose">
@@ -114,17 +154,6 @@ function Chat() {
             </ul>
           </div>
         )}
-        <div className="rounded-md border p-4">
-          <ol className="prose">
-            {config.workflow.map((w, idx) => (
-              <li key={idx} className="line-clamp-3">
-                <span className="mr-2">Step {idx + 1}:</span>
-                {w.done && <span className="mr-2">✅</span>}
-                <span>{w.system_prompt}</span>
-              </li>
-            ))}
-          </ol>
-        </div>
       </div>
     </div>
   )
